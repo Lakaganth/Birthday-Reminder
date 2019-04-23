a9642bce893ec5c19812fea2e9327205ad1d2811
Lakaganth <lakaganth89@gmail.com>
2019-04-23 08:43:37 +0530

    LS and Delete

--- app.js
+++ app.js
@@ -7,16 +7,23 @@ const list = document.querySelector(".collections");
 loadAllListners();
 
 function loadAllListners() {
+  //Dom Load event
+  document.addEventListener("DOMContentLoaded", getbabies);
+
   textInput.addEventListener("submit", addBday);
   list.addEventListener("click", delBday);
 }
 
-function addBday(e) {
-  fn = firstName.value;
-  ln = lastName.value;
-  bday = birthday.value;
+// Loading From Local Storage
+function getbabies() {
+  let babies;
+  if (localStorage.getItem("babies") === null) {
+    babies = [];
+  } else {
+    babies = JSON.parse(localStorage.getItem("babies"));
+  }
 
-  if (fn != "" && ln != "" && bday != "") {
+  babies.forEach(baby => {
     const li = document.createElement("li");
     const row = document.createElement("div");
     const col = document.createElement("div");
@@ -43,11 +50,54 @@ function addBday(e) {
     li.appendChild(row);
     list.appendChild(li);
 
-    cardTitle.appendChild(document.createTextNode(`${fn}  ${ln}`));
-    cardContent.appendChild(document.createTextNode(`${bday}`));
+    cardTitle.appendChild(document.createTextNode(`${baby.fn}  ${baby.ln}`));
+    cardContent.appendChild(document.createTextNode(`${baby.bday}`));
+  });
+}
+
+function addBday(e) {
+  const user = {
+    fn: firstName.value,
+    ln: lastName.value,
+    bday: birthday.value
+  };
+
+  console.log(user);
+
+  if (user.fn != "" && user.ln != "" && user.bday != "") {
+    const li = document.createElement("li");
+    const row = document.createElement("div");
+    const col = document.createElement("div");
+    const cardBG = document.createElement("div");
+    const cardContent = document.createElement("div");
+    const cardTitle = document.createElement("span");
+    const del = document.createElement("a");
+
+    li.className = "collection-item";
+    row.className = "row";
+    col.className = "col s12 m6";
+    cardBG.className = "card blue darken-1";
+    cardContent.className = "card-content white-text";
+    cardTitle.className = "card-title";
+    del.className = "delete-item  secondary-content";
+
+    del.innerHTML = '<i class="fas fa-times"></i>';
+
+    cardTitle.appendChild(del);
+    cardContent.appendChild(cardTitle);
+    cardBG.appendChild(cardContent);
+    col.appendChild(cardBG);
+    row.appendChild(col);
+    li.appendChild(row);
+    list.appendChild(li);
+
+    cardTitle.appendChild(document.createTextNode(`${user.fn}  ${user.ln}`));
+    cardContent.appendChild(document.createTextNode(`${user.bday}`));
   } else {
     alert("Input all details");
   }
+
+  storeToLocalStorage(user);
 
   e.preventDefault();
 }
@@ -55,13 +105,58 @@ function addBday(e) {
 // Delete the birthday
 
 function delBday(e) {
-  console.log(
+  const test =
     e.target.parentElement.parentElement.parentElement.parentElement
-      .parentElement
-  );
+      .parentElement;
+
+  console.log(test.firstChild.firstChild.firstChild);
+
   if (e.target.parentElement.classList.contains("delete-item")) {
     if (confirm("Are you sure?")) {
       e.target.parentElement.parentElement.parentElement.parentElement.parentElement.remove();
+
+      //Remove from LS
+      removeBabyFromLS(
+        e.target.parentElement.parentElement.parentElement.parentElement
+          .parentElement
+      );
     }
   }
+}
+
+// ADD USER to LS
+
+function storeToLocalStorage(bday) {
+  let babies;
+  if (localStorage.getItem("babies") === null) {
+    babies = [];
+  } else {
+    babies = JSON.parse(localStorage.getItem("babies"));
+  }
+
+  babies.push(bday);
+
+  localStorage.setItem("babies", JSON.stringify(babies));
+}
+
+// Remove user from LS
+
+function removeBabyFromLS(delbaby) {
+  let babies;
+  if (localStorage.getItem("babies") === null) {
+    babies = [];
+  } else {
+    babies = JSON.parse(localStorage.getItem("babies"));
+  }
+
+  babies.forEach((baby, index) => {
+    const dl = `${baby.fn}  ${baby.ln}`;
+    console.log(delbaby.firstChild.firstChild.firstChild.textContent);
+    if (delbaby.firstChild.firstChild.firstChild.textContent === dl) {
+      babies.splice(index, 1);
+      console.log("Del Success");
+    }
+  });
+
+  localStorage.setItem("babies", JSON.stringify(babies));
 }
